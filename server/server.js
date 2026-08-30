b2baf7f91e49adc6563e8568459f26b0598e1d35
Tripti Rawat <triptirawat2008@gmail.com>
2026-08-30 22:52:58 +0530

    Fix backend environment path

--- server/server.js
+++ server/server.js
@@ -5,8 +5,7 @@ import pg from "pg";
 import { ethers } from "ethers";
 
 
-dotenv.config({ path: "../.env" });
-
+dotenv.config({ path: ".env" });
 const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
 
 const contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
